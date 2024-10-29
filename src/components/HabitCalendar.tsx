9f90697eeedc0c95f4c9bd57643dd95d3ea5b822
aryanbhat <aryanbhat324@gmail.com>
2024-10-29 23:35:12 +0530

    fixed habits sideways

--- src/components/HabitCalendar.tsx
+++ src/components/HabitCalendar.tsx
@@ -145,7 +145,7 @@ export default function HabitCalendar(props: { data: HabitValue }) {
             />
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 place-items-center gap-4 pt-4 border-t  border-border">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 pt-4 border-t  border-border">
           {data.longestStreak && (
             <StatCard
               icon={<TrendingUpIcon className="w-6 h-6" />}
